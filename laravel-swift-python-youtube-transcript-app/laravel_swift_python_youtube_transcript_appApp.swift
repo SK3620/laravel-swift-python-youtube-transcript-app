8d355e2c2d37430479fee1c32696333637859add
SK3620 <k.n.t11193620@gmail.com>
2024-08-06 08:52:17 +0900

    Initial Commit

---
+++ laravel-swift-python-youtube-transcript-app/laravel_swift_python_youtube_transcript_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  laravel_swift_python_youtube_transcript_appApp.swift
+//  laravel-swift-python-youtube-transcript-app
+//
+//  Created by 鈴木 健太 on 2024/08/06.
+//
+
+import SwiftUI
+
+@main
+struct laravel_swift_python_youtube_transcript_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
